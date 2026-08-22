a7ca617fa545418acc4499db9e626279bfa6dee9
David Adewunmi <pycodegenius@gmail.com>
2026-08-22 22:36:15 +0100

    feat": Update PredictionRequest and PredictionReponse for data validation

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field, field_validator
 
-class MessagePayload(BaseModel):
-    message:str
+class PredictionRequest(BaseModel):
+    text: str = Field(..., example="Congratulations! You've won a free $1,000 gift card. Click here.")
+
+    @field_validator("text")
+    @classmethod
+    def validate_text(cls, value):
+        if not value.strip():
+            raise ValueError("Text must contain readable characters.")
+        return value
+    
+class PredictionResponse(BaseModel):
+    prediction: str
+    confidence_score: float
+
